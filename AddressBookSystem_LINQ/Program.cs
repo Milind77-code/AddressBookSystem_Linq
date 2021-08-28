1f39e891169e9cc2ec29356babbcb387541e091a
Milind_77 <milinddhote77@gmail.com>
2021-08-28 09:17:44 +0530

    [Milind] Add : Ability to create database to store contacts.

---
+++ AddressBookSystem_LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookSystem_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Welcome_To_AddressBookSystem_Linq***");
+            AddrssBookModel model = new AddrssBookModel();
+            AddressBookDataTable dataTable = new AddressBookDataTable();
+            dataTable.CreateTable(model);
+        }
+    }
+}
